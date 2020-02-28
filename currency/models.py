61576692560b159851beb8250bf15fbc94e7991a
dath <drive.grisha.off@gmail.com>
2020-02-28 13:14:52 +0700

    make Currency

--- currency/models.py
+++ currency/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Currency(models.Model):
+    name = models.CharField(max_length=6)
+    rate = models.FloatField()
+
+    def __str__(self):
+        return str(self.name) + ": " + str(self.rate)
+
+    def get_rate(self):
+        return {
+            'name': self.name,
+            'rate': self.rate
+        }
