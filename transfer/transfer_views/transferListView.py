048e4fe228505b9e495a45860be7eee110682a40
dath <drive.grisha.off@gmail.com>
2020-02-28 13:06:15 +0700

    Сделал транзакции, Список транзакций со спикском пользователей

--- transfer/transfer_views/transferListView.py
+++ transfer/transfer_views/transferListView.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-
+from django.db.models import Q
 from transfer.models import Transfer
 from transfer.transfer_serializers.list import TransferListSerializer
 
@@ -12,7 +12,7 @@ class TransferListView(APIView):
     authentication_classes = [TokenAuthentication]
 
     def get(self, request):
-        t_list = Transfer.objects.all()
+        t_list = Transfer.objects.filter(Q(from_user=request.user) | Q(to_user=request.user))
         serializer = TransferListSerializer(t_list, many=True)
         return Response({"transfers": serializer.data})
 
